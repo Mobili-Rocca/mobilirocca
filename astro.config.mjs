077039408e8563d159236901eb1d53f7f41ad958
Lorenzo Pantano <l.pantano97@gmail.com>
2026-06-08 11:03:47 +0200

    feat: seo integration and improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.mobilirocca.it',
+  integrations: [sitemap()],
   i18n: {
     defaultLocale: 'it',
     locales: ['it', 'en'],
